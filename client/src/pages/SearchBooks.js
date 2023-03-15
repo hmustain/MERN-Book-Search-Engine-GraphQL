6454d01ea0c4b544fcb676f5f5858f02c649c3ee
hmustain <hunter@bunkerbranding.com>
2023-03-15 12:44:12 -0500

    updating errors

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -8,6 +8,7 @@ import {
   Row
 } from 'react-bootstrap';
 
+import { useMutation } from '@apollo/react-hooks';
 import Auth from '../utils/auth';
 import {  searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
@@ -21,6 +22,8 @@ const SearchBooks = () => {
 
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
+
+  const [saveBook] = useMutation(SAVE_BOOK);
 
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
