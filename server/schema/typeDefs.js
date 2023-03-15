d22aea3807733ef4d0b3c8ea733b21bcc71b2f01
hmustain <hunter@bunkerbranding.com>
2023-03-15 10:30:28 -0500

    added code efor typeDefs.js based of book and user models

---
+++ server/schema/typeDefs.js
@@ -0,0 +1,48 @@
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
+    type Book {
+       authors: [String]
+       description: String!
+       bookId: String!
+       image: String
+       link: String
+       title: String!
+        }
+
+      type User {
+        id: ID!
+        username: String!
+        email: String!
+        password: String!
+        savedBooks: [Book]
+        bookCount: Int
+        }
+
+    type Auth {
+        token: ID!
+        user: User
+        }
+
+    type Query {
+    me: User
+    }
+
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(bookData: BookInput!): User
+        removeBook(bookId: String!): User
+    }
+
+    input BookInput {
+        authors: [String]
+        description: String!
+        bookId: String!
+        image: String
+        link: String
+        title: String!
+    }
+    `;
+
+module.exports = typeDefs
